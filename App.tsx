5fb352066213d86bed62bc9adc53fed4169658ab
nhattinh2601 <nhattinh2601@gmail.com>
2024-03-06 17:53:51 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,125 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useRef, useState } from "react";
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const carouseItem = require("./assets/carousel.json");
+const { width, height } = Dimensions.get("window");
+const viewConfigRef = { viewAreaCoveragePercentThreshold: 95 };
+
+interface CarouselItems {
+  title: string;
+  url: string;
+  promo: string;
+}
+
+export default function App() {
+  let flatListRef = useRef<FlatList<CarouselItems> | null>();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  //only needed if want to know the index
+  const onViewRef = useRef(({ changed }: { changed: any }) => {
+    if (changed[0].isViewable) {
+      setCurrentIndex(changed[0].index);
+    }
+  });
+
+  const scrollToIndex = (index: number) => {
+    flatListRef.current?.scrollToIndex({animated: true, index: index})
+  }
+
+  const renderItems: React.FC<{ item: CarouselItems }> = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => console.log("clicked")}
+        activeOpacity={1}
+      >
+        <Image source={{ uri: item.url }} style={styles.image} />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>{item.title}</Text>
+          <Text style={styles.footerText}>{item.promo}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <FlatList
+        data={carouseItem}
+        renderItem={renderItems}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        ref={(ref) => {
+          flatListRef.current = ref;
+        }}
+        style={styles.carousel}
+        viewabilityConfig={viewConfigRef}
+        onViewableItemsChanged={onViewRef.current}
+      />
+      <View style={styles.dotView}>
+        {carouseItem.map(({}, index: number) => (
+          <TouchableOpacity
+            key={index.toString()}
+            style={[
+              styles.circle,
+              { backgroundColor: index == currentIndex ? "black" : "grey" },
+            ]}
+            onPress={() => scrollToIndex(index)}
+          />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  carousel: {
+    maxHeight: 300,
+  },
+  image: {
+    width,
+    height: 250,
+    resizeMode: "cover",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 50,
+    paddingHorizontal: 40,
+    alignContent: "center",
+    backgroundColor: "#000",
+  },
+  footerText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  dotView: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 20,
+  },
+  circle: {
+    width: 10,
+    height: 10,
+    backgroundColor: "grey",
+    borderRadius: 50,
+    marginHorizontal: 5,
+  },
+});
